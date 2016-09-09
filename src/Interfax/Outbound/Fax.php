90534858465fd51ce7689eadf1fdbe42dc2e34c8
Mike Smith <mike.smith@camc-ltd.co.uk>
2016-09-09 14:32:48 +0100

    + stubbed Fax class

---
+++ src/Interfax/Outbound/Fax.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Interfax
+ *
+ * (C) InterFAX, 2016
+ *
+ * @package interfax/interfax
+ * @author Interfax <dev@interfax.net>
+ * @copyright Copyright (c) 2016, InterFAX
+ * @license MIT
+ */
+
+
+namespace Interfax\Outbound;
+
+
+class Fax
+{
+    public function __construct($params = [])
+    {
+
+    }
+}
